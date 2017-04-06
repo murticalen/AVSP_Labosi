306c0160d43de1f1559a2cef675af1a1758881ea
Alen Murtić <Murta-thelegend@hotmail.com>
2017-04-06 22:40:00 +0200

    SimHashBuckets
    Finished the 2nd part

--- src/SimHashBuckets.java
+++ src/SimHashBuckets.java
@@ -1,35 +1,44 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
 public class SimHashBuckets {
 	
+	public static final int b = 8;
+	public static final int r = 128/b;
+	
 	public static void main(String[] args) throws Exception {
 		
 		//System.setOut(new PrintStream(new File("C:\\FER\\out.txt")));
+		//System.setIn(new FileInputStream(new File("C:\\FER predmeti\\8. semestar\\AVSP\\Labosi\\Lab01\\lab1B_primjer\\test2\\R.in")));
 		
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		
 		String line = reader.readLine();
 		int textCount = Integer.parseInt(line);
 		
-		List<String> hashes = new ArrayList<>(textCount);
+		String[] hashes = new String[textCount];
 		for(int i = 0; i < textCount; i++){
 			line = reader.readLine();
-			hashes.add(simhash(line));
+			hashes[i] = simhash(line);
 		}
+		
+		List<Set<Integer>> lsh = LSH(hashes);
 		
 		line = reader.readLine();
 		int queryCount = Integer.parseInt(line);
 		
 		for(int i = 0; i < queryCount; i++){
 			String[] query = reader.readLine().split(" ");
-			LSH(Integer.parseInt(query[0]), Integer.parseInt(query[1]), hashes);
+			difference(Integer.parseInt(query[0]), Integer.parseInt(query[1]), hashes, lsh);
 		}
-		
 	}
 	
 	public static String simhash(String line){
@@ -58,19 +67,48 @@ public class SimHashBuckets {
 		return sb.toString();
 	}
 	
-	public static void LSH(int I, int K, List<String> hashes){
-		String hash = hashes.get(I);
+	public static List<Set<Integer>> LSH(String[] hashes){
+		List<Set<Integer>> candidates = new ArrayList<>();
+		for(int i = 0; i < b; i++){
+			Map<Integer, Set<Integer>> area = new HashMap<>();
+			for(int ID = 0; ID < hashes.length; ID++){
+				candidates.add(new HashSet<>());
+				int val = hash2Int(i, hashes[ID]);
+				Set<Integer> texts;
+				if(area.containsKey(val)){
+					texts = area.get(val);
+					for(int text_ID : texts){
+						candidates.get(ID).add(text_ID);
+						candidates.get(text_ID).add(ID);
+					}
+				}
+				else{
+					texts = new HashSet<>();
+				}
+				texts.add(ID);
+				area.put(val, texts);
+			}
+		}
+		return candidates;
+	}
+	
+	public static int hash2Int(int pos, String hash){
+		return Integer.parseInt(hash.substring(r * pos, r* pos + r), 2);
+	}
+	
+	public static void difference(int I, int K, String[] hashes, List<Set<Integer>> candidates){
 		int count = 0;
-		
-		for(int i = 0; i < hashes.size(); i++){
-			if(i == I) continue;
+		char[] hash = hashes[I].toCharArray();
+		for(int text_id : candidates.get(I)){
 			int difference = 0;
-			for(int j = 0; j < 128; j++){
-				if(hash.charAt(j) != hashes.get(i).charAt(j)){
+			for(int i = 0; i < 128; i++){
+				if(hashes[text_id].charAt(i) != hash[i]){
 					difference++;
 				}
 			}
-			if(difference <= K) count++;			
+			if(difference <= K){
+				count++;
+			}
 		}
 		System.out.println(count);
 	}
